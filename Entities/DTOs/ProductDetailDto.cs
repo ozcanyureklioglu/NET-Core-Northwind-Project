13615391d4358d2c209dd6a4d7c9d6b32188a58c
Özcan Yüreklioğlu <ozcan.yu@hotmail.com>
2022-09-19 13:05:25 +0300

    DTO eklendi

---
+++ Entities/DTOs/ProductDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Core.Entities;
+namespace Entities.DTOs
+{
+    public class ProductDetailDto:IDto
+    {
+        public string category_name { get; set; }
+
+        public string product_name { get; set; }
+
+        public short units_in_stock { get; set; }
+
+        public int units_on_order { get; set; }
+
+        public int unit_price { get; set; } 
+    }
+}
+
